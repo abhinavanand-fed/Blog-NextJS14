ce523efe68156a898fd0ceacff255bf5eeb9612b
abhinavanand-fed <abhinavanand080@gmail.com>
2024-04-06 19:27:48 +0530

    first commit

---
+++ config/site.ts
@@ -0,0 +1,13 @@
+export const siteConfig = {
+  name: "AbhinavBlog",
+  url: "https://example.com",
+  description: "Nextjs 14 blog using velite, tailwind and shadcn",
+  author: "Abhinav Anand",
+  links: {
+    twitter: "https://twitter.com/abh1nav_anand",
+    github: "https://github.com/abhinavanand007",
+    personalSite: "http://localhost:3000",
+  },
+};
+
+export type SiteConfig = typeof siteConfig;
